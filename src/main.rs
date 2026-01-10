9414b962ef42be7a267e929b25b109ecee229bf7
Marco Guerreiro <mascanho@sapo.pt>
2026-01-10 22:27:38 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -135,6 +135,8 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
                                 KeyCode::Char('l') | KeyCode::Right => app.next_detail_tab(),
                                 KeyCode::Tab => app.next_detail_tab(),
                                 KeyCode::BackTab => app.previous_detail_tab(),
+                                KeyCode::Char('k') | KeyCode::Up => app.previous_row(),
+                                KeyCode::Char('j') | KeyCode::Down => app.next_row(),
                                 _ => {}
                             }
                             continue;
